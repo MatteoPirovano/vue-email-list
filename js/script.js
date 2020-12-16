3889f52f5b5ff576c074b433b67665c9f49929f2
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-12-16 18:19:02 +0100

    Fine

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 var email = new Vue ({
   el:'#root',
   data: {
-    message:'Hello World'
+    mails: []
   },
-  mounted: {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(function (response) {
-      console.log(response.data;)
-    });
+  methods: {},
+  mounted: function() {
+    const self = this
+    for (var i = 0; i <9; i++) {
+      axios
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(function (response) {
+        self.mails.push(response.data.response)
+      })
+    }
   }
-
 });
